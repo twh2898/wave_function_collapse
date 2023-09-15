2da6dd2b1b1aba53e188e091ba271c8dec91577c
Thomas Harrison <twh2898@vt.edu>
2023-09-14 21:03:59 -0400

    Move code to main

--- main.py
+++ main.py
@@ -1,206 +1,155 @@
 #!/usr/bin/env python3
 
-from random import randint, choice
+import sys
+import colorama
+from wfc import Grid, propagate, Tile, Cell, collapse
+from typing import TextIO
 
-letter = 'ABCDEFGHIJ'
 
-SHOW_SOLVE = True
+def clean_hex(n: int):
+    """Return a 2 character hex string for integer n with space padding."""
+    return '{:2s}'.format(hex(n)[2:])
 
 
-class Cell:
-    value: int | None = None
+def load_rules():
+    # Water, Beach, Grass, Mountain (gray)
+    pass
 
-    def __init__(self, r, c):
-        self.r = r
-        self.c = c
-        self.options = list(range(1, 10))
 
-    def __repr__(self):
-        if SHOW_SOLVE:
-            if self.value:
-                return str(self.value)
+def load_grid(w: int, h: int):
+    """Create a grid of width w and height h."""
+    cells = {}
+    for y in range(h):
+        for x in range(w):
+            options = [t for t in Tile]
+            cell = Cell(x, y, options)
+            cells[(x, y)] = cell
+    return Grid(w, h, cells)
+
+
+def init_grid(grid: Grid):
+    """Set the borders of grid to water and add a stone to the center."""
+    for x in range(grid.w):
+        cell = grid[(x, 0)]
+        cell.value = Tile.Water
+        cell.options.clear()
+        propagate(cell, grid, None)
+
+        cell = grid[(x, grid.h-1)]
+        cell.value = Tile.Water
+        cell.options.clear()
+        propagate(cell, grid, None)
+
+    for y in range(grid.h):
+        cell = grid[(0, y)]
+        cell.value = Tile.Water
+        cell.options.clear()
+        propagate(cell, grid, None)
+
+        cell = grid[(grid.w-1, y)]
+        cell.value = Tile.Water
+        cell.options.clear()
+        propagate(cell, grid, None)
+
+    cell = grid[(grid.w//2, grid.h//2)]
+    cell.value = Tile.Stone
+    cell.options.clear()
+    propagate(cell, grid, None)
+
+
+cmap = {
+    Tile.Water: colorama.Fore.CYAN,
+    Tile.Sand: colorama.Fore.YELLOW,
+    Tile.Grass: colorama.Fore.GREEN,
+    Tile.Stone: colorama.Fore.BLACK,
+    None: colorama.Fore.RED,
+}
+
+
+def print_grid(grid: Grid, f: TextIO | None = sys.stdout):
+    """Print grid to a file stream."""
+    for y in range(grid.h):
+        for x in range(grid.w):
+            cell = grid[(x, y)]
+            if cell.is_solved():
+                print(cmap[cell.value] + "██" +
+                      colorama.Fore.RESET, end="", file=f)
             else:
-                return '?'
-        else:
-            if self.value:
-                return '*'
-            else:
-                return str(len(self.options))
-
-    def remove_option(self, option):
-        if option in self.options:
-            self.options.remove(option)
-
-    def entropy(self):
-        return len(self.options)
-
-    def complete(self):
-        return self.entropy() == 0
+                mask = 0
+                for v in cell.options:
+                    mask |= v.value
+                print(clean_hex(mask) + colorama.Fore.RESET, end="", file=f)
+        print(file=f)
+    print(file=f)
 
 
-class Sudoku:
-    def __init__(self):
-        self._cells: list[list[Cell]] = []
-        self._flat: list[Cell] = []
-
-        for r in range(9):
-            row = []
-            for c in range(9):
-                cell = Cell(r, c)
-                row.append(cell)
-                self._flat.append(cell)
-            self._cells.append(row)
-
-    def __repr__(self):
-        lines = ''
-        for r in range(9):
-            for c in range(9):
-                cell = self.get_cell(r, c)
-                lines += ' {}'.format(cell)
-            lines += '\n'
-        return lines
-
-    def is_solved(self):
-        for cell in self._flat:
-            if cell.value is None:
-                return False
-        return True
-
-    def get_cell(self, r, c):
-        assert 0 <= r <= 9
-        assert 0 <= c <= 9
-        return self._cells[r][c]
-
-    def get_min_entropy(self):
-        def entropy(cell: Cell):
-            return cell.entropy()
-
-        self._flat.sort(key=entropy)
-        cells = [c for c in self._flat if c.entropy() > 0]
-        assert len(cells) > 0
-        lowest_entropy = cells[0].entropy()
-
-        i = 0
-        for i, cell in enumerate(cells):
-            if cell.entropy() > lowest_entropy:
-                break
-
-        sub_cells = cells[:i+1]
-        return choice(sub_cells)
-
-    def collapse(self, target: Cell):
-        assert len(target.options) > 0
-        target.value = choice(target.options)
-        target.options.clear()
-
-    def propagate(self, target: Cell):
-        for c in range(9):
-            if c == target.c:
-                continue
-            cell = self.get_cell(target.r, c)
-            cell.remove_option(target.value)
-
-        for r in range(9):
-            if r == target.r:
-                continue
-            cell = self.get_cell(r, target.c)
-            cell.remove_option(target.value)
-
-        r_block = target.r % 3
-        c_block = target.c % 3
-
-        for r in range(3):
-            r += r_block * 3
-            for c in range(3):
-                c += c_block * 3
-
-                if r == target.r and c == target.c:
-                    continue
-
-                cell = self.get_cell(r, c)
-                cell.remove_option(target.value)
-
-    def solve(self):
-        while not self.is_solved():
-            cell = self.get_min_entropy()
-            self.collapse(cell)
-            self.propagate(cell)
-
-            for cell in self._flat:
-                if cell.value is None and cell.entropy() == 1:
-                    self.collapse(cell)
-                    self.propagate(cell)
+def print_grids(grids: list[Grid], h: int, f: TextIO | None = sys.stdout):
+    """Print multiple grids to a file stream."""
+    for y in range(h):
+        for g in grids:
+            for x in range(g.w):
+                cell = g[(x, y)]
+                print(cmap[cell.value] + "██" +
+                      colorama.Fore.RESET, end="", file=f)
+            print('  ', end='', file=f)
+        print(file=f)
 
 
-# def solve(sudoku: Sudoku):
-#     cells = []
-#     for row in sudoku._cells:
-#         cells += row
+def main_multi(w, h, rules):
+    """Generate multiple grids."""
+    for _ in range(5):
+        grids = []
+        for _ in range(3):
+            grid = load_grid(w, h)
+            init_grid(grid)
 
-#     cell = choice(cells)
-#     cells.remove(cell)
-#     cell.fill()
-#     collapse(cell, sudoku)
-#     for cell in cells:
-#         assert len(cell.options) > 0
-#         if len(cell.options) == 1:
-#             cell.value = cell.options[0]
-#             cell.options.clear()
-#     cells = list(filter(lambda c: not c.complete(), cells))
+            while not grid.is_solved():
+                collapse(grid, rules)
 
-#     while cells:
-#         cells.sort(key=lambda c: c.entropy())
-#         low_entropy = cells[0].entropy()
-#         sub_cells = list(filter(lambda c: c.entropy() == low_entropy, cells))
-#         cell = choice(sub_cells)
-#         cells.remove(cell)
-#         cell.fill()
-#         collapse(cell, sudoku)
-#         for cell in cells:
-#             assert len(cell.options) > 0
-#             if len(cell.options) == 1:
-#                 cell.value = cell.options[0]
-#                 cell.options.clear()
-#         cells = list(filter(lambda c: not c.complete(), cells))
+            grids.append(grid)
+
+        print()
+        print_grids(grids, h)
 
 
-def test(sudoku: Sudoku):
-    assert sudoku.is_solved()
+def main(w, h, rules, debug=False, f=None):
+    """Generate a single grid."""
+    rules = load_rules()
 
-    for row in sudoku._cells:
-        for cell in row:
-            assert cell.value is not None, f'Cell {cell.r} {cell.c} is None'
-            assert cell.entropy() == 0
+    grid = load_grid(w, h)
+    init_grid(grid)
+    if debug:
+        if f:
+            f.write('0\n')
+        print_grid(grid, f)
+        print_grid(grid)
 
-    for r in range(9):
-        r_vals = [sudoku.get_cell(r, c).value for c in range(9)]
-        for i in range(1, 10):
-            assert i in r_vals
+    i = 1
+    while not grid.is_solved():
+        collapse(grid, rules)
+        if debug:
+            if f:
+                f.write(str(i) + '\n')
+            print_grid(grid, f)
+            print_grid(grid)
+        i += 1
 
-    for c in range(9):
-        c_vals = [sudoku.get_cell(r, c).value for r in range(9)]
-        for i in range(1, 10):
-            assert i in c_vals
-
-    for r_block in range(3):
-        for c_block in range(3):
-            block_cells = []
-
-            for r in range(3):
-                r += r_block * 3
-                for c in range(3):
-                    c += c_block * 3
-
-                    cell = sudoku.get_cell(r, c)
-                    block_cells.append(cell.value)
-
-            for i in range(1, 10):
-                assert i in block_cells
+    print_grid(grid)
 
 
 if __name__ == '__main__':
-    s = Sudoku()
-    s.solve()
-    # solve(s)
-    print(s)
-    test(s)
+    for i in range(16):
+        s = Tile.whoami(i)
+        print(clean_hex(i), '=', s)
+
+    # rules = rules_from_img(img)
+    rules = load_rules()
+
+    use_multi = True
+
+    if use_multi:
+        main_multi(16, 16, rules)
+
+    else:
+        with open('debug', 'w') as f:
+            main(16, 16, rules, debug=True, f=f)
